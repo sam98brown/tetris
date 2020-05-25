193fe13f568b8b8bb700d3e89f8fb5d3736b8e5f
sam98brown <sam98brown@gmail.com>
2020-05-25 22:52:49 +0100

    init

---
+++ app.js
@@ -0,0 +1,167 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10
+
+    //Tetromino time
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const zTetromino = [
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1]
+    ]
+
+    const tTetromino = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+let currentPosition = 4
+let currentRotation = 0
+
+//randomly select a tetromino
+let random = Math.floor(Math.random() * theTetrominoes.length); 
+console.log(random)
+
+let current = theTetrominoes[random][currentRotation]
+
+//draw the tetromino
+function draw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetromino')
+    })
+}
+
+//remove the tetromino
+function undraw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.remove('tetromino')
+    })
+}
+
+//make the tetromino move down every second
+setInterval(moveDown, 300)
+
+//assign functions to keyCodes
+function control(e) {
+    if(e.keyCode === 37) {
+        moveLeft()
+    } else if (e.keyCode === 38) {
+        rotate()
+    } else if(e.keyCode === 39) {
+        moveRight()
+    } else if(e.keyCode === 40) {
+        moveDown()
+    }
+}
+document.addEventListener('keyup', control)
+
+//move down function
+
+function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+}
+
+function freeze() {
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+        current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+        //startt a new tetromino falling
+        random = Math.floor(Math.random() * theTetrominoes.length)
+        current = theTetrominoes[random][currentRotation]
+        currentPosition = 4
+        draw()
+        clearLine()
+    }
+}
+
+function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+    if(!isAtLeftEdge) {
+        currentPosition -=1
+    } 
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition +=1
+    }
+
+    draw()
+    
+}
+
+
+function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+
+    if(!isAtRightEdge) {
+        currentPosition +=1
+    } 
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition -=1
+    }
+
+    draw()
+    
+}
+
+function rotate() {
+    undraw()
+    currentRotation++
+    if(currentRotation === current.length) {
+        currentRotation = 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    draw()
+}
+
+
+function clearLine() {
+    for (let i = 0; i < 199; i+=width) {
+        const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+        console.log(row)
+        if(row.every(index => squares[index].classList.contains('taken'))) {
+            row.forEach(index => {
+                squares[index].classList.remove('taken')
+            })
+        }
+    }
+}
+
+
+
+
+//draw()
+
+    //console.log(squares)
+})
